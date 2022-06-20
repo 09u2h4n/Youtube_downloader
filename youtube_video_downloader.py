8a3777d126426c26ad9abe05db272ef1e01b7634
Oğuzhan <99614395+09u2h4n@users.noreply.github.com>
2022-06-20 11:46:02 +0300

    Add files via upload

--- youtube_video_downloader.py
+++ youtube_video_downloader.py
@@ -1,17 +1,15 @@
 from subprocess import getoutput
-from sys import exit
 from os import getcwd, rename
 
 """
 TODO
 *Channel and Searching system will be added
-
 *System will be optimized
 *Useless things will be deleted
 """
 
 class Downloader(object):
-    def get_module():
+    def __get_module(self):
         """If you have moduler error or something about modules use this method."""
         pip_list = getoutput("pip list")
         if "pytube" not in pip_list.split():
@@ -23,16 +21,23 @@ class Downloader(object):
         else:
             print("Please install manually!")
     
-    get_module()
+    __get_module(self="self")
 
     def __doc__(self):
         print("""
 Usage :
-Download video for;
-obj = Downloader("https://youtu.be/m4gnMWua4xo").download_video()
+Downloading video with highest resolution for;
+>>Downloader("https://youtu.be/m4gnMWua4xo").download_video()
+Downloading video with special resolution for:
+>>Downloader("https://youtu.be/m4gnMWua4xo").download_video(res="360p")
 or 
 Download audio for;
-obj = Downloader("https://youtu.be/m4gnMWua4xo").download_audio()
+>>Downloader("https://youtu.be/m4gnMWua4xo").download_audio()
+or 
+Download Playlist videos for;
+>>Downloader("https://youtube.com/playlist?list=PLUNPjaQ-i_voID9V4u9L23XWiwpUPY0sG").download_playlist()
+Download Playlist just video's audio for;
+>>Downloader("https://youtube.com/playlist?list=PLUNPjaQ-i_voID9V4u9L23XWiwpUPY0sG").download_playlist(audio = True)
         """)
 
     def __init__(self, url):
@@ -48,30 +53,23 @@ obj = Downloader("https://youtu.be/m4gnMWua4xo").download_audio()
             pl = Playlist(self.url)
             self.pl = pl
 
-    def download_video(self):
-        try:
+    def download_video(self, res="0"):
+        if res == "0":
             print("Downloading..")
             self.yt.streams.get_highest_resolution().download()
             print(f"Downloaded in {self.pwd}")
-        except:
-            print("Command line download method started..")
-            getoutput(f"pytube {self.url}")
+        elif res != "0":
+            print("Downloading..")
+            try:
+                self.yt.streams.get_by_resolution(res).download()
+            except AttributeError:
+                print(f"Could not find: {res}")
             print(f"Downloaded in {self.pwd}")
         
     def download_audio(self):
-        try:
-            print("Downloading..")
-            self.yt.streams.get_audio_only().download(filename=f"{self.yt.title}.mp3")
-            print(f"Downloaded in {self.pwd}")
-        except:
-            print("Command line download method started..")
-            getoutput(f"pytube {self.url} -a")
-            try:
-                rename(f"{self.yt.title}.mp4", f"{self.yt.title}.mp3")
-            except FileExistsError:
-                print(f"Cannot create an existing file ('mp3' file could not be downloaded). The file named '{self.yt.title}'.mp4 has been downloaded")
-                return
-            print(f"Downloaded in {self.pwd}")
+        print("Downloading..")
+        self.yt.streams.get_audio_only().download(filename=f"{self.yt.title}.mp3")
+        print(f"Downloaded in {self.pwd}")
 
     def download_playlist(self, audio = False):
         video_num = 0
@@ -89,5 +87,3 @@ obj = Downloader("https://youtu.be/m4gnMWua4xo").download_audio()
                 video.streams.get_audio_only().download(output_path=f"{self.pl.title} Audios", filename=f"{video_num}){video.title.replace('/', '')}.mp3")
                 print(f"Downloaded '{video.title}.mp3'")
             print(f"Downloaded all videos in {self.pwd} ==> {self.pl.title}")
-
-#Downloader("https://youtube.com/playlist?list=PLUNPjaQ-i_voID9V4u9L23XWiwpUPY0sG").download_playlist(audio=True)
